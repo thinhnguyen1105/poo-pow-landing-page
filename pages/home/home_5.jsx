8746e4db2c79c315035a63d90fd907db8de39c38
thinhnguyen1105 <thinhnguyen.teamx@gmail.com>
2022-10-01 19:02:32 +0700

    init project

---
+++ pages/home/home_5.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Collection_category from "../../components/collectrions/collection_category";
+import {
+  Feature_collections,
+  HeadLine,
+  Partners,
+  Bids,
+} from "../../components/component";
+import Meta from "../../components/Meta";
+import Hero_5 from "../../components/hero/hero_5";
+import Process from "../../components/blog/process";
+import FilterCategoryItem from "../../components/categories/filterCategoryItem";
+import Download from "../../components/blog/download";
+import Story from "../../components/about/story";
+import Team from "../../components/about/team";
+import About_news from "../../components/blog/about_news";
+
+const Home_5 = () => {
+  return (
+    <>
+      <Meta title="PooPow | The World's Largest Digital Marketplace For NFT" />
+      <Hero_5 />
+      <Bids />
+      <Process />
+      <Story compFor="about" />
+      <Team />
+      <Partners />
+      <About_news />
+    </>
+  );
+};
+
+export default Home_5;
---
+++ tailwind.config.js
@@ -0,0 +1,123 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
+  theme: {
+    screens: {
+      sm: "575px",
+      md: "768px",
+      lg: "1025px",
+      xl: "1202px",
+    },
+    fontFamily: {
+      display: ['"CalSans-SemiBold"', "sans-serif"],
+      body: ['"DM Sans"', "sans-serif"],
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      accent: "#fbb000",
+      "accent-dark": "#ffa544",
+      "accent-light": "#ffba7c",
+      "accent-lighter": "#ffd8a0",
+      "light-base": "#faf8f5",
+      green: "#83a100",
+      orange: "#FEB240",
+      red: "#d9222f",
+      blue: "#1b2e8c",
+      jacarta: {
+        base: "#79685a",
+        50: "#d9d8d7",
+        100: "#cac5c0",
+        200: "#b6aba2",
+        300: "#a69587",
+        400: "#917a67",
+        500: "#7e6652",
+        600: "#664d38",
+        700: "#583f2b",
+        800: "#42250f",
+        900: "#2d1b0d",
+      },
+    },
+    boxShadow: {
+      none: "none",
+      sm: "0px 1px 2px 0px rgba(13, 16, 45, 0.1)",
+      base: "0px 1px 2px -1px rgba(13, 16, 45, 0.1), 0px 2px 4px 0px rgba(13, 16, 45, 0.1)",
+      md: "0px 2px 4px -2px rgba(13, 16, 45, 0.1), 0px 4px 6px -1px rgba(13, 16, 45, 0.1)",
+      lg: "0px 4px 6px -4px rgba(13, 16, 45, 0.1), 0px 10px 15px -3px rgba(13, 16, 45, 0.1)",
+      xl: "0px 8px 10px -6px rgba(13, 16, 45, 0.1), 0px 20px 25px -5px rgba(13, 16, 45, 0.1)",
+      "2xl":
+        "0px 25px 50px -12px rgba(13, 16, 45, 0.1), 0px 12px 24px 0px rgba(13, 16, 45, 0.1)",
+      "accent-volume":
+        "5px 5px 10px rgba(108, 106, 213, 0.25), inset 2px 2px 6px #f0ca8d, inset -5px -5px 10px #e49c36",
+      "white-volume":
+        "5px 5px 10px rgba(108, 106, 212, 0.25), inset 2px 2px 6px #f9f5ee, inset -5px -5px 10px #efe9df",
+    },
+    fontSize: {
+      xs: ["0.75rem", { lineHeight: "normal" }],
+      "2xs": ["0.8125rem", { lineHeight: "normal" }],
+      sm: ["0.875rem", { lineHeight: "normal" }],
+      base: ["1rem", { lineHeight: "normal" }],
+      lg: ["1.25rem", { lineHeight: "normal" }],
+      xl: ["1.5rem", { lineHeight: "normal" }],
+      "2xl": ["1.75rem", { lineHeight: "normal" }],
+      "3xl": ["2rem", { lineHeight: "normal" }],
+      "4xl": ["2.25rem", { lineHeight: "normal" }],
+      "5xl": ["2.5rem", { lineHeight: "normal" }],
+      "6xl": ["3.5rem", { lineHeight: "normal" }],
+      "7xl": ["4.25rem", { lineHeight: "normal" }],
+    },
+    borderRadius: {
+      none: "0",
+      sm: "0.125rem",
+      DEFAULT: "0.25rem",
+      DEFAULT: "4px",
+      md: "0.375rem",
+      lg: "0.5rem",
+      full: "9999px",
+      large: "12px",
+    },
+
+    extend: {
+      borderRadius: {
+        "2lg": "0.625rem",
+      },
+      transitionProperty: {
+        height: "height",
+        width: "width",
+      },
+      animation: {
+        fly: "fly 6s cubic-bezier(0.75, 0.02, 0.31, 0.87) infinite",
+        heartBeat: "heartBeat 1s cubic-bezier(0.75, 0.02, 0.31, 0.87) infinite",
+        progress: "progress 5s linear",
+      },
+      keyframes: {
+        fly: {
+          "0%, 100%": { transform: "translateY(5%)" },
+          "50%": { transform: "translateY(0)" },
+        },
+        heartBeat: {
+          "0%, 40%, 80%, 100%": { transform: "scale(1.1)" },
+          "20%, 60%": { transform: "scale(.8)" },
+        },
+        progress: {
+          "0%": { width: "0%" },
+          "100%": { width: "100%" },
+        },
+      },
+    },
+    namedGroups: ["dropdown"],
+  },
+  // variants: {
+  // 	display: ['children', 'children-not'],
+  // },
+  plugins: [],
+};
